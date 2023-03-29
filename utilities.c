cdb5d860a09e5875b3dc6dbaaec38219adc09dbd
jmaouche <maouchejoseph@gmail.com>
2023-03-29 21:29:22 +0200

    Added utilities.c file with basic sort functions

---
+++ utilities.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+// Fonction pour échanger deux éléments
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// Fonction pour partitionner le tableau et retourner l'index du pivot
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Fonction pour trier le tableau en utilisant QuickSort
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+//Utilistation du Quicksort + Agencement du Zéro à la fin de la liste 
+void special_sort(int arr[], int low, int high) {
+    quicksort(arr, low, high-1);
+    int temp_arr[high];
+    for (int i = 1; i < high; i++)
+    {
+        temp_arr[i-1] = arr[i];
+    }
+    for (int i = 0; i < high; i++)
+    {
+        arr[i] = temp_arr[i];
+    }
+}
+
+void print_arr(int arr[],int n){
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
